309b42c0168128221dd9f4c3d1b5806c12d736b1
ghobot <greg@sciencelifeny.com>
2017-08-28 15:31:26 -0400

    store JSON data in index file; parse json in markers script

--- examples/js/markers-component.js
+++ examples/js/markers-component.js
@@ -3,9 +3,10 @@
 AFRAME.registerComponent('markers', {
 
     init: function() {
+        var data = document.getElementById('data').innerHTML;
+        var markerData = JSON.parse(data);
 
         var scene = document.querySelector('a-scene');
-        // in a loop create a-plane, set events, add class, add material, link to image, set position, set scale, make it look at camera
         var checkPointsContainer,
             infoText,
             infoTarget,
@@ -27,45 +28,15 @@ AFRAME.registerComponent('markers', {
             textWrapCount = "20",
             offset = 2,
             visible = "false",
-            //_details = document.getElementById('details'),
             _markerTitle = document.getElementById('marker-title'),
             _markerDetails = document.getElementById('marker-details'),
             imageClicked = false,
             touchMsg = "Touch 'i' for more Information.";
 
-
-        var markerData = {
-            "markers": [
-                { "name": "gown", "description": "single gown use", "position": { x: 0, y: 3, z: -4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "cap", "description": "disposable cap", "position": { x: 0, y: 5, z: -4.5 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "textiles", "description": "non-recyclable Sms-Polypropylene textiles", "position": { x: 4, y: 2.5, z: -4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "tray", "description": "reusable tray but with disposable components", "position": { x: -3, y: 2, z: -4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "packaging", "description": "single-use packaging", "position": { x: -2.8, y: 1, z: -4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "robot-console", "description": "robot console reusable", "position": { x: -4.5, y: 2, z: -4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "robot-arm", "description": "robotic arm plastic wrap - municipal solid waste", "position": { x: -5, y: 4, z: -3.5 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "waste-bag", "description": "red waste bag", "position": { x: -4.5, y: 2, z: -4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "surgical-robot", "description": "reusable surgical robot", "position": { x: -6, y: 7, z: -1.5 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "bunny-suit", "description": "bunny suit - disposable", "position": { x: -4, y: 3, z: 2 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "led-lighting", "description": "overhead LED lighting", "position": { x: -5, y: 9, z: 0 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "cautery", "description": "monitoring equipment with cautery station", "position": { x: -1, y: 4, z: 4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "drape-hoses", "description": "warm air drape hoses are reusable, the rest are not", "position": { x: -0.5, y: 3, z: 5 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "underlayer", "description": "disposable: polypropylene fabric underlayer fluid boundary absorbant plastic layer on top", "position": { x: 0, y: 1, z: 3 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "waste", "description": "municipal solid waste", "position": { x: 1.2, y: 3, z: 4 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "textiles2", "description": "non-recyclable Sms-Polypropylene textiles", "position": { x: 1, y: 2, z: 3 }, "hasDetails": false, "details_image": "", "details_text": "" },
-                { "name": "AF6", "description": "Image of objects on surface", "position": { x: 1, y: 3, z: -4 }, "hasDetails": true, "details_image": "images/AF6_image_1.jpeg", "details_text": "" },
-                { "name": "BF6", "description": "Items on tray", "position": { x: -5, y: 3, z: -1.5 }, "hasDetails": true, "details_image": "images/BF6_image_2.jpg", "details_text": "" },
-                { "name": "DAU", "description": "Items on tray", "position": { x: -5, y: 3, z: -1.5 }, "hasDetails": true, "details_image": "images/DAu_image_4.jpg", "details_text": "" },
-                { "name": "EAU", "description": "Items on tray", "position": { x: -5, y: 3, z: -1.5 }, "hasDetails": true, "details_image": "images/EAu_image_3.jpg.jpg", "details_text": "" },
-            ]
-        }
-        //console.log("names: " + markerData.markers[0].name);
-
         var infoMarkersContainer = document.createElement('a-entity');
         var _detailsContainer = document.getElementById('details-content');
 
         for (var i = 0; i < markerData.markers.length; i++) {
-
-            //console.log("names: " + markerData.markers[i].name);
 
             var _infoPosition = markerData.markers[i].position;
             var _infoText = markerData.markers[i].description;
@@ -77,10 +48,7 @@ AFRAME.registerComponent('markers', {
             var _infoMarkerEl = document.createElement("a-plane"),
                 _infoTextEl = document.createElement("a-text"),
                 _infoImageEl = document.createElement("a-plane"),
-                _hasDetails = markerData.markers[i].hasDetails; //does the position have images or more data
-
-            
-
+                _hasDetails = markerData.markers[i].hasDetails; //does the position have images or more data         
 
             // format info icon hotspot    
             _infoMarkerEl.setAttribute("event-set__enter", _eventEnter);
@@ -93,7 +61,6 @@ AFRAME.registerComponent('markers', {
             _infoMarkerEl.setAttribute("src", image);
             //_infoMarkerEl.setAttribute("cursor-listener");
 
-
             _infoTextEl.setAttribute("id", _infoTarget);
             _infoTextEl.setAttribute("align", alignment);
             _infoTextEl.setAttribute("color", black);
@@ -105,24 +72,20 @@ AFRAME.registerComponent('markers', {
 
             if (_hasDetails) { // make message for more info marker 
 
-
                 _infoTextEl.setAttribute("value", touchMsg);
                 _infoTextEl.setAttribute("geometry", textGeometryImage);
 
                 _infoMarkerEl.setAttribute("data-index", i);
-                
-                console.log("index: " , _infoMarkerEl.getAttribute("data-index"));
+
+
 
                 _infoMarkerEl.addEventListener('click', function(evt) {
-                    //console.log('I was clicked at: ', evt.detail.intersection.point);
-                var _index = this.getAttribute("data-index");    
-                
-                console.log("clicked index: " , _index);
+
+                    var _index = this.getAttribute("data-index");
+
                     var _imageSrc = markerData.markers[_index].details_image,
                         _imageDescription = markerData.markers[_index].description,
                         _imageName = markerData.markers[_index].name;
-
-                    console.log("image source: ", _imageSrc);
 
                     imageClicked = true;
 
@@ -165,70 +128,5 @@ AFRAME.registerComponent('markers', {
             infoMarkersContainer.appendChild(_infoMarkerEl);
         }
         scene.appendChild(infoMarkersContainer);
-
-    },
-
-    update: function() {
-
-
-
-
     },
 });
-
-// const infoPositions = [
-//     { x: 0, y: 3, z: -4 }, // gown
-//     { x: 0, y: 5, z: -4.5 }, //  cap               
-//     { x: 4, y: 2.5, z: -4 }, //  textiles            
-//     { x: -3, y: 2, z: -4 }, //tray
-//     { x: -2.8, y: 1, z: -4 }, //packaging
-//     { x: -4.5, y: 2, z: -4 }, //robot console
-//     { x: -5, y: 4, z: -3.5 }, //robot arm
-//     { x: -5, y: 2.1, z: -0.5 }, //red waste bag
-//     { x: -6, y: 7, z: -1.5 }, //surgical robot
-//     { x: -4, y: 3, z: 2 }, //bunny suit
-//     { x: -5, y: 9, z: 0 }, //  LED lighting
-//     { x: -1, y: 4, z: 4 }, //cautery
-//     { x: -0.5, y: 3, z: 5 }, //drape hoses
-//     { x: 0, y: 1, z: 3 }, //fabric underlayer
-//     { x: 1.2, y: 3, z: 4 }, //solid waste 
-//     { x: 1, y: 2, z: 3 }, //textiles 2
-// ];
-
-// const infoTextArray = [
-//     "single gown use", //gown 
-//     "disposable cap", //cap
-//     "non-recyclable Sms-Polypropylene textiles", //textiles
-//     "reusable tray but with disposable components", //tray
-//     "single-use packaging", //packaging
-//     "robot console reusable", //robot console
-//     "robotic arm plastic wrap - municipal solid waste", //robot arm
-//     "red waste bag", //red waste bag
-//     "reusable surgical robot", //surgical robot
-//     "bunny suit - disposable", //bunny suit
-//     "overhead LED lighting", //LED lighting
-//     "monitoring equipment with cautery station", //cautery
-//     "warm air drape hoses are reusable, the rest are not", //drape hoses
-//     "disposable: polypropylene fabric underlayer fluid boundary absorbant plastic layer on top", // fabric underlayer
-//     "municipal solid waste", //solid waste
-//     "non-recyclable Sms-Polypropylene textiles", //textiles 2
-// ];
-
-// const infoTargetNames = [
-//     "gown",
-//     "cap",
-//     "textiles",
-//     "tray",
-//     "packaging",
-//     "robot-console",
-//     "robot-arm",
-//     "waste-bag",
-//     "surgical-robot",
-//     "bunny-suit",
-//     "led-lighting",
-//     "cautery",
-//     "drape-hoses",
-//     "underlayer",
-//     "waste",
-//     "textiles2",
-// ];
